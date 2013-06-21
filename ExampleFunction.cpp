bed87ebe538a0740d1c15b034761549fd7bc714c
KbearXD <kbearxd@kbearxd-K40IN.(none)>
2013-06-21 15:22:37 +0800

    加入LSE wirelength model 但z的部份用簡單的centerZ()帶過而已

--- ExampleFunction.cpp
+++ ExampleFunction.cpp
@@ -1,25 +1,108 @@
 #include "ExampleFunction.h"
+#include <math.h>
+#include <iostream>
 
-// minimize 3*x^2 + 2*x*y + 2*y^2 + 7
-
-ExampleFunction::ExampleFunction()
+ExampleFunction::ExampleFunction(Placement &placement):_placement(placement)
 {
 }
 
 void ExampleFunction::evaluateFG(const vector<double> &x, double &f, vector<double> &g)
 {
-    f = 3*x[0]*x[0] + 2*x[0]*x[1] + 2*x[1]*x[1] + 7; // objective function
-    g[0] = 6*x[0] + 2*x[1]; // gradient function of X
-    g[1] = 2*x[0] + 4*x[1]; // gradient function of Y
+   f = 0;
+   unsigned num = _placement.numModules();
+   double r = 0.01*(_placement.boundryRight() - _placement.boundryLeft());
+
+   for(unsigned i = 0;i<3*num;++i)
+      g[i] = 0;
+
+
+   for(unsigned i = 0 ; i<_placement.numNets();++i){
+      double f_x_1=0.0,f_x_2=0.0,f_y_1=0.0,f_y_2=0.0,f_z_1=0.0,f_z_2=0.0;
+      vector<unsigned> moduleID;
+      Net& net = _placement.net(i);
+      for(unsigned j = 0; j<net.numPins();++j){
+         moduleID.push_back(net.pin(j).moduleId());
+      }
+
+      for(unsigned k = 0; k<moduleID.size();++k){
+         double xi,yi,zi;
+         if(_placement.module(moduleID[k]).isFixed()){
+            Module& module = _placement.module(moduleID[k]);
+            xi = (module.centerX()+net.pin(k).xOffset());
+            yi = (module.centerY()+net.pin(k).yOffset());
+            zi = (module.centerZ());
+         }    
+         else{
+            xi = (x[moduleID[k]]+net.pin(k).xOffset());
+            yi = (x[moduleID[k]+num]+net.pin(k).yOffset());
+            zi = (x[moduleID[k]+2*num]);
+         }
+
+         f_x_1 = f_x_1 + exp( xi /r);     //exp( (module.centerX()+net.pin(k).xOffset()) /r)
+         f_x_2 = f_x_2 + exp((-1*xi)/r);  //exp((-1*(module.centerX()+net.pin(k).xOffset()))/r);
+         f_y_1 = f_y_1 + exp( yi /r);     //exp((module.centerY()+net.pin(k).yOffset())/r);
+         f_y_2 = f_y_2 + exp((-1*yi)/r);  //exp((-1*(module.centerY()+net.pin(k).yOffset()))/r);
+         f_z_1 = f_z_1 + exp( zi /r);
+         f_z_2 = f_z_2 + exp((-1*zi)/r);
+
+      }
+      f = f + log(f_x_1) + log(f_x_2) + log(f_y_1) + log(f_y_2) + log(f_z_1) + log(f_z_2);
+
+      for(unsigned k=0; k<moduleID.size();++k){
+         if(_placement.module(moduleID[k]).isFixed()==false){
+            g[moduleID[k]]     += ( (exp((x[moduleID[k]]+net.pin(k).xOffset())/r))*(1/f_x_1) + (exp((-1*(x[moduleID[k]]+net.pin(k).xOffset()))/r)*(-1))*(1/f_x_2) ); 
+            g[moduleID[k]+num] += ( (exp((x[moduleID[k]+num]+net.pin(k).yOffset())/r))*(1/f_y_1) + (exp((-1*(x[moduleID[k]+num])+net.pin(k).xOffset())/r)*(-1))*(1/f_y_2) );
+            g[moduleID[k]+2*num]+=( (exp((x[moduleID[k]+2*num])/r))*(1/f_z_1) + (exp((-1*(x[moduleID[k]+2*num]))/r)*(-1))*(1/f_z_2) );
+         }
+      }
+
+   }
+   f = f * r;
 }
 
 void ExampleFunction::evaluateF(const vector<double> &x, double &f)
 {
-    f = 3*x[0]*x[0] + 2*x[0]*x[1] + 2*x[1]*x[1] + 7; // objective function
+   f = 0;
+   unsigned num = _placement.numModules();
+   double r = 0.01*(_placement.boundryRight() - _placement.boundryLeft());
+
+   for(unsigned i = 0 ; i<_placement.numNets();++i){
+      double f_x_1=0.0,f_x_2=0.0,f_y_1=0.0,f_y_2=0.0,f_z_1=0.0,f_z_2=0.0;
+      vector<unsigned> moduleID;
+      Net& net = _placement.net(i);
+      for(unsigned j = 0; j<net.numPins();++j){
+         moduleID.push_back(net.pin(j).moduleId());
+      }
+
+      for(unsigned k = 0; k<moduleID.size();++k){
+         double xi,yi,zi;
+         if(_placement.module(moduleID[k]).isFixed()){
+            Module& module = _placement.module(moduleID[k]);
+            xi = (module.centerX()+net.pin(k).xOffset());
+            yi = (module.centerY()+net.pin(k).yOffset());
+            zi = (module.centerZ());
+         }    
+         else{
+            xi = (x[moduleID[k]]+net.pin(k).xOffset());
+            yi = (x[moduleID[k]+num]+net.pin(k).yOffset());
+            zi = (x[moduleID[k]+2*num]);
+         }
+
+         f_x_1 = f_x_1 + exp( xi /r);     //exp( (module.centerX()+net.pin(k).xOffset()) /r)
+         f_x_2 = f_x_2 + exp((-1*xi)/r);  //exp((-1*(module.centerX()+net.pin(k).xOffset()))/r);
+         f_y_1 = f_y_1 + exp( yi /r);     //exp((module.centerY()+net.pin(k).yOffset())/r);
+         f_y_2 = f_y_2 + exp((-1*yi)/r);  //exp((-1*(module.centerY()+net.pin(k).yOffset()))/r);
+         f_z_1 = f_z_1 + exp( zi /r);
+         f_z_2 = f_z_2 + exp((-1*zi)/r);
+
+      }
+      f = f + log(f_x_1) + log(f_x_2) + log(f_y_1) + log(f_y_2) + log(f_z_1) + log(f_z_2);
+   }
+   f = f * r;
 }
 
 unsigned ExampleFunction::dimension()
 {
-    return 2; // num_blocks*2 
+    return  num_blocks*3;
     // each two dimension represent the X and Y dimensions of each block
 }
--- include/ExampleFunction.h
+++ include/ExampleFunction.h
@@ -6,11 +6,13 @@
 class ExampleFunction : public NumericalOptimizerInterface
 {
 public:
-    ExampleFunction();
+    ExampleFunction(Placement &placement);
 
     void evaluateFG(const vector<double> &x, double &f, vector<double> &g);
     void evaluateF(const vector<double> &x, double &f);
     unsigned dimension();
 
+private:
+    Placement& _placement;
 };
 #endif // EXAMPLEFUNCTION_H
