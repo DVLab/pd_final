ad93efada867333d62da30cf575ec6d74515b02f
Kidd7325 <kidd7325@kidd7325-VirtualBox.(none)>
2013-06-23 01:19:14 +0800

    density func(not finished yet)

--- density.cpp
+++ density.cpp
@@ -1,5 +1,6 @@
 #include "ExampleFunction.h"
 #include <math.h>
+#include <cmath>
 #include <iostream>
 
 ExampleFunction::ExampleFunction(Placement &placement,LayerMgr &layer):_placement(placement),_layer(layer)
@@ -19,39 +20,31 @@ void ExampleFunction::evaluateFG(const vector<double> &x, double &f, vector<doub
 	double a_y, b_y;
 	double a_z, b_z;
 
-	double binNum = 81.0;
+	double binNum = 1.0;
 	double binWidth = (_placement.boundryRight() - _placement.boundryLeft()) / sqrt(binNum);
 	//cout<<"binWidth "<<binWidth<<endl;
 	double binHeight = (_placement.boundryTop() - _placement.boundryBottom()) / sqrt(binNum);
 	//cout<<"binHeight "<<binHeight<<endl;
-	double M = 1.0*(binWidth*binHeight - 0); // assume t_density = 1.0 , area of preplaced block = 0
+	double M = 0*(binWidth*binHeight - 0); // assume t_density = 1.0 , area of preplaced block = 0
 	//cout<<"M "<<M<<endl;
 
 	vector<double> binCenterX,binCenterY,binCenterZ;
 	for(int i = 0 ; i < sqrt(binNum) ; i++){
-		binCenterX.push_back(_placement.boundryLeft() + (2*i+1)*((_placement.boundryRight() - _placement.boundryLeft()/ (sqrt(binNum)*2))));
-		binCenterY.push_back(_placement.boundryTop() - (2*i+1)*((_placement.boundryTop() - _placement.boundryBottom()/ (sqrt(binNum)*2))));
+		binCenterX.push_back(_placement.boundryLeft() + (2*i+1)*((_placement.boundryRight() - _placement.boundryLeft())/ (sqrt(binNum)*2)));
+		binCenterY.push_back(_placement.boundryTop() - (2*i+1)*((_placement.boundryTop() - _placement.boundryBottom())/ (sqrt(binNum)*2)));
 		binCenterZ.push_back(0 + (2*i+1)*0.5);
 	}
 
-	for(size_t i = 0 ; i < binCenterX.size() ; i++)
-		cout<<binCenterX[i]<<" ";
-	cout<<endl;
-	for(size_t i = 0 ; i < binCenterY.size() ; i++)
-		cout<<binCenterY[i]<<" ";
-	cout<<endl;
-	for(size_t i = 0 ; i < binCenterZ.size() ; i++)
-		cout<<binCenterZ[i]<<" ";
-	cout<<endl;
-
-	for(unsigned y = 0 ; y < sqrt(binNum) ; y++){
-		for(unsigned z = 0 ; z < sqrt(binNum) ; z++){
+	// bin[x][y][z]
+	for(unsigned z = 0 ; z < sqrt(binNum) ; z++){
+		for(unsigned y = 0 ; y < sqrt(binNum) ; y++){
 			for(unsigned i = 0 ; i < sqrt(binNum) ; i++){
+				D = 0;
 				for(unsigned j = 0 ; j < num ; j++){
 					// x coordinate
 					a_x = 4/((_placement.module(j).width() + 2*binWidth)*(_placement.module(j).width() + 4*binWidth));
 					b_x = 2/(binWidth*(_placement.module(j).width() + 4*binWidth));
-					dx = x[j] - binCenterX[i];
+					dx = abs(x[j] - binCenterX[i]);
 					if(dx >= 0 && dx <= _placement.module(j).width()/2 + binWidth){
 						Px = 1 - a_x*dx*dx;
 						dPx = -2*a_x*dx;
@@ -67,7 +60,7 @@ void ExampleFunction::evaluateFG(const vector<double> &x, double &f, vector<doub
 					// y coordinate
 					a_y = 4/((_placement.module(j).height() + 2*binHeight)*(_placement.module(j).height() + 4*binHeight));
 					b_y = 2/(binHeight*(_placement.module(j).height() + 4*binHeight));
-					dy = x[j+num] - binCenterY[z];
+					dy = abs(x[j+num] - binCenterY[y]);
 					if(dy >= 0 && dy <= _placement.module(j).height()/2 + binHeight){
 						Py = 1 - a_y*dy*dy;
 						dPy = -2*a_y*dy;
@@ -83,20 +76,20 @@ void ExampleFunction::evaluateFG(const vector<double> &x, double &f, vector<doub
 					// z coordinate
 					a_z = 4/15;
 					b_z = 2/5;
-					dz = x[j+2*num] - binCenterZ[y];
+					dz = abs(x[j+2*num] - binCenterZ[z]);
 					if(dz >= 0 && dz <= 1.5){
 						Pz = 1 - a_z*dz*dz;
 						dPz = -2*a_z*dz;
 					}
 					else if (dz >= 2.5){
 						Pz = 0;
-						dPz = 0;
+						dPz = 1;//0;
 					}
 					else{
 						Pz = b_z*(dz - 2.5)*(dz - 2.5);
 						dPz = 2*b_z*(dz - 2.5);
 					}
-		
+					cout<<dx<<"  "<<dy<<"  "<<dz<<"  "<<dPx<<"  "<<dPy<<"  "<<dPz<<endl;
 					D += Px*Py*Pz;
 					g[j] = 2*(D - M)*Py*Pz*dPx;
 					g[j+num] = 2*(D-M)*Px*Pz*dPy;
@@ -106,7 +99,6 @@ void ExampleFunction::evaluateFG(const vector<double> &x, double &f, vector<doub
 			}
 		}
 	}
-
 }
 
 void ExampleFunction::evaluateF(const vector<double> &x, double &f)
@@ -115,9 +107,8 @@ void ExampleFunction::evaluateF(const vector<double> &x, double &f)
    unsigned num = _placement.numModules();
    double dx,dy,dz;
    double Px,Py,Pz;
-   double dPx,dPy,dPz; 
+   //double dPx,dPy,dPz; 
    double D = 0;
-	double M; // D--density function  M--density target
 
 	double a_x, b_x;
 	double a_y, b_y;
@@ -125,71 +116,76 @@ void ExampleFunction::evaluateF(const vector<double> &x, double &f)
 
 	double binNum = 81.0;
 	double binWidth = (_placement.boundryRight() - _placement.boundryLeft()) / sqrt(binNum);
+	//cout<<"binWidth "<<binWidth<<endl;
 	double binHeight = (_placement.boundryTop() - _placement.boundryBottom()) / sqrt(binNum);
+	//cout<<"binHeight "<<binHeight<<endl;
+	double M = 0*(binWidth*binHeight - 0); // assume t_density = 1.0 , area of preplaced block = 0
+	//cout<<"M "<<M<<endl;
 
 	vector<double> binCenterX,binCenterY,binCenterZ;
 	for(int i = 0 ; i < sqrt(binNum) ; i++){
-		binCenterX.push_back(_placement.boundryLeft() + (2*i+1)*((_placement.boundryRight() - _placement.boundryLeft()/ (sqrt(binNum)*2))));
-		binCenterY.push_back(_placement.boundryTop() - (2*i+1)*((_placement.boundryTop() - _placement.boundryBottom()/ (sqrt(binNum)*2))));
+		binCenterX.push_back(_placement.boundryLeft() + (2*i+1)*((_placement.boundryRight() - _placement.boundryLeft())/ (sqrt(binNum)*2)));
+		binCenterY.push_back(_placement.boundryTop() - (2*i+1)*((_placement.boundryTop() - _placement.boundryBottom())/ (sqrt(binNum)*2)));
 		binCenterZ.push_back(0 + (2*i+1)*0.5);
 	}
 
-	for(unsigned y = 0 ; y < sqrt(binNum) ; y++){
-		for(unsigned z = 0 ; z < sqrt(binNum) ; z++){
+	// bin[x][y][z]
+	for(unsigned z = 0 ; z < sqrt(binNum) ; z++){
+		for(unsigned y = 0 ; y < sqrt(binNum) ; y++){
 			for(unsigned i = 0 ; i < sqrt(binNum) ; i++){
+				D = 0;
 				for(unsigned j = 0 ; j < num ; j++){
 					// x coordinate
 					a_x = 4/((_placement.module(j).width() + 2*binWidth)*(_placement.module(j).width() + 4*binWidth));
 					b_x = 2/(binWidth*(_placement.module(j).width() + 4*binWidth));
-					dx = x[j] - binCenterX[i];
+					dx = abs(x[j] - binCenterX[i]);
 					if(dx >= 0 && dx <= _placement.module(j).width()/2 + binWidth){
 						Px = 1 - a_x*dx*dx;
-						dPx = -2*a_x*dx;
+						//dPx = -2*a_x*dx;
 					}
 					else if (dx >= (_placement.module(j).width()/2 + 2*binWidth)){
 						Px = 0;
-						dPx = 0;
+						//dPx = 0;
 					}
 					else{
 						Px = b_x*(dx - _placement.module(j).width()/2 - 2*binWidth)*(dx - _placement.module(j).width()/2 - 2*binWidth);
-						dPx = 2*b_x*(dx - _placement.module(j).width()/2 - 2*binWidth);
+						//dPx = 2*b_x*(dx - _placement.module(j).width()/2 - 2*binWidth);
 					}
 					// y coordinate
 					a_y = 4/((_placement.module(j).height() + 2*binHeight)*(_placement.module(j).height() + 4*binHeight));
 					b_y = 2/(binHeight*(_placement.module(j).height() + 4*binHeight));
-					dy = x[j+num] - binCenterY[z];
+					dy = abs(x[j+num] - binCenterY[y]);
 					if(dy >= 0 && dy <= _placement.module(j).height()/2 + binHeight){
 						Py = 1 - a_y*dy*dy;
-						dPy = -2*a_y*dy;
+						//dPy = -2*a_y*dy;
 					}
 					else if (dy >= (_placement.module(j).height()/2 + 2*binHeight)){
 						Py = 0;
-						dPy = 0;
+						//dPy = 0;
 					}
 					else{
 						Py = b_y*(dy - _placement.module(j).height()/2 - 2*binHeight)*(dy - _placement.module(j).height()/2 - 2*binHeight);
-						dPy = 2*b_y*(dy - _placement.module(j).height()/2 - 2*binHeight);
+						//dPy = 2*b_y*(dy - _placement.module(j).height()/2 - 2*binHeight);
 					}
 					// z coordinate
 					a_z = 4/15;
 					b_z = 2/5;
-					dz = x[j+2*num] - binCenterZ[y];
+					dz = abs(x[j+2*num] - binCenterZ[z]);
 					if(dz >= 0 && dz <= 1.5){
 						Pz = 1 - a_z*dz*dz;
-						dPz = -2*a_z*dz;
+						//dPz = -2*a_z*dz;
 					}
 					else if (dz >= 2.5){
 						Pz = 0;
-						dPz = 0;
+						//dPz = 0;
 					}
 					else{
 						Pz = b_z*(dz - 2.5)*(dz - 2.5);
-						dPz = 2*b_z*(dz - 2.5);
+						//dPz = 2*b_z*(dz - 2.5);
 					}
 		
-					D += Px*Py;//*Pz;
+					D += Px*Py*Pz;
 				}
-				M = 1.0*(binWidth*binHeight - 0); // assume t_density = 1.0 , area of preplaced block = 0
 				f += (D - M)*(D - M);
 			}
 		}
